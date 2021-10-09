839b9615ca82b052d83d9e6e6d891e9fbf320da9
Sapphire2664 <aditya.keshan2019@vitstudent.ac.in>
2021-10-09 16:52:49 +0530

    Added pagination and testing

--- hello.go
+++ hello.go
@@ -3,9 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 	"sync"
+	"testing"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -87,6 +91,8 @@ func posts(w http.ResponseWriter, r *http.Request) {
 }
 func getAllPosts(w http.ResponseWriter, r *http.Request) {
 	lock.Lock()
+	option := options.Find()
+	option.SetLimit(2)
 	var val string
 	para := r.URL.Path
 	if len(para) > 6 {
@@ -94,9 +100,8 @@ func getAllPosts(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(val)
 	}
 	if r.Method == "GET" && val != "" {
-
 		var data []bson.M
-		cursor, err := collectionPosts.Find(ctx, bson.M{"userId": val})
+		cursor, err := collectionPosts.Find(ctx, bson.M{"userId": val}, option)
 		if err != nil {
 			panic(err)
 		} else {
@@ -108,6 +113,54 @@ func getAllPosts(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	lock.Unlock()
+}
+func TestPostingUserDetails(t *testing.T){
+	Body := strings.NewReader({"id": "1234", "name": "Aditya Keshan", 
+	"email": "aditya.keshan2019@vitstudent.ac.in", "password": "ABcd123@@"})
+		req, err := http.NewRequest("POST", "http://localhost:3000/users/", Body)
+		if(err!=nil){
+			fmt.Println("Error in Posting");
+		}else{
+			fmt.Println("Success in testing:",req.Close);
+		}
+}
+func TestPostingPostsDetails(t *testing.T){
+	
+	Body := strings.NewReader({"id": "1234", "caption": "Be the change", 
+		"url": "https://www.google.com", "timestamp": "16-47","userId":"1234"})
+	req, err := http.NewRequest("POST", "http://localhost:3000/posts/", Body)
+	if(err!=nil){
+		fmt.Println("Error in Posting");
+	}else{
+		fmt.Println("Success in testing:",req.Close);
+	}
+}
+func TestGetUserDetails(t *testing.T)
+{
+	req, err := http.NewRequest("GET", "http://localhost:3000/users/1234", nil)
+	if(err!=nil){
+		fmt.Println("Error in Get");
+	}else{
+		fmt.Println("Success in testing:",req.Close);
+	}
+}
+func TestGetPostDetails(t *testing.T)
+{
+	req, err := http.NewRequest("GET", "http://localhost:3000/posts/1234", nil)
+	if(err!=nil){
+		fmt.Println("Error in Get");
+	}else{
+		fmt.Println("Success in testing:",req.Close);
+	}
+}
+func TestGetUsersAllPostsDetails(t *testing.T)
+{
+	req, err := http.NewRequest("GET", "http://localhost:3000/posts/users/1234", nil)
+	if(err!=nil){
+		fmt.Println("Error in Get");
+	}else{
+		fmt.Println("Success in testing:",req.Close);
+	}
 }
 func handleRequests() {
 	http.HandleFunc("/users/", homePage)
